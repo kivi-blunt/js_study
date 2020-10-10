027a0aa6af86b8ffba4f253354542362db9fb8a4
vladislav kazak <vladik.zakaz@gmail.com>
2020-10-10 15:24:28 +0300

    Усложненное задание

--- script/script.js
+++ script/script.js
@@ -1,6 +1,20 @@
-let money = 100, income = 200, addExpenses = 1, 
- mission = 'true', period = 1;  
-const deposit = 200;
+let money = 100; //любое число “Доход за месяц”, 
+let income = 'Фриланс'; //строка с дополнительными доходом 
+let addExpenses = 'Такси, интернет'; //строка дополнительных расходов 
+let deposit = true; // любое булево значение
+let mission = 1000; //любое число
+let period = 1; //число от 1 до 12 (месяцев) 
+let budgetDay = 1000; //дневной бюджет
 
-alert("Hello");
-console.log('Hello');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+
+console.log("Период равен " + period + " Месяцев");
+console.log("Цель зарабоать " + mission + " рублей/долларов");
+
+
+console.log(addExpenses.toLowerCase().split(","));
+console.log(budgetDay);
